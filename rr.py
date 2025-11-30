71a3f47b9f66473358adf9d206b73aa336b23fa0
alaabahaaahmed21-dotcom <alaa.bahaaahmed21@gmail.com>
2025-11-30 16:29:41 +0200

    Update rr.py

--- rr.py
+++ rr.py
@@ -75,7 +75,7 @@ BILINGUAL_LABELS = {
     "Federation": "Federation / الاتحاد",
     "Enter Club for all players": "Enter Club for all players / أدخل النادي لجميع اللاعبين",
     "Enter Nationality for all players": "Enter Nationality for all players / أدخل الجنسية لجميع اللاعبين", 
-    "Enter Coach Name for all players": "Enter Coach Name  / أدخل اسم المدرب 
+    "Enter Coach Name for all players": "Enter Coach Name  / أدخل اسم المدرب "
     "Enter Phone Number for the Coach": "Enter Phone Number for the Coach / أدخل رقم هاتف المدرب",
     "Number of players to add:": "Number of players to add: / عدد اللاعبين المراد إضافتهم",
     "Choose course type:": "Choose course type: / اختر نوع الدورة",
